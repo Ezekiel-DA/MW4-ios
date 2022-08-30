78192436c552186e4cfb0144a2f88270969ef011
Nicolas LEFEBVRE <nicolas.lefebvre.lp@gmail.com>
2022-08-30 17:00:10 -0400

    Initial Commit

---
+++ MW4-Savannah/MW4_SavannahApp.swift
@@ -0,0 +1,17 @@
+//
+//  MW4_SavannahApp.swift
+//  MW4-Savannah
+//
+//  Created by Nicolas LEFEBVRE on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MW4_SavannahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
